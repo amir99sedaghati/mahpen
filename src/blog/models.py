f1e749064d9d07087f77dc280e634a317c96385e
mojtabaakbari221b <mojtaba.akbari.221b@gmail.com>
2022-01-02 15:02:53 +0330

    add blog model

--- src/blog/models.py
+++ src/blog/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from ckeditor.fields import RichTextField
 
-# Create your models here.
+class Category(models.Model):
+    title = models.CharField(max_length=256)
+    image = models.ImageField(upload_to="category/image")
+    is_promote = models.BooleanField(default=False)
+
+class Post(models.Model):
+    title = RichTextField(max_length=1024)
+    text = RichTextField()
+    image = models.ImageField(upload_to="post/image")
+    is_promote = models.BooleanField(default=False)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
