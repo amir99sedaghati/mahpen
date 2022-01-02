b20367f96a7981d318233b118fc1110a0ca372fa
mojtabaakbari221b <mojtaba.akbari.221b@gmail.com>
2022-01-02 15:17:08 +0330

    edit model support date

--- src/blog/models.py
+++ src/blog/models.py
@@ -5,6 +5,10 @@ class Category(models.Model):
     title = models.CharField(max_length=256)
     image = models.ImageField(upload_to="category/image")
     is_promote = models.BooleanField(default=False)
+    date_publish = models.DateField(auto_now=True)
+
+    def __str__(self):
+        return f'{__class__.__name__}({self.id} , {self.title})'
 
 class Post(models.Model):
     title = RichTextField(max_length=1024)
@@ -12,3 +16,7 @@ class Post(models.Model):
     image = models.ImageField(upload_to="post/image")
     is_promote = models.BooleanField(default=False)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    date_publish = models.DateField(auto_now=True)
+    
+    def __str__(self):
+        return f'{__class__.__name__}({self.id} , {self.title})'
