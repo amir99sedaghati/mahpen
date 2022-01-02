58a2376fd3e1ce10905932807926324e1171d0e5
mojtabaakbari221b <mojtaba.akbari.221b@gmail.com>
2022-01-02 14:43:06 +0330

    support static and media

--- src/mahpen/settings.py
+++ src/mahpen/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'ckeditor',
     'blog',
 ]
 
@@ -117,7 +118,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+import os
+
+STATIC_ROOT = os.path.join(Path(BASE_DIR).resolve().parent, 'static')
+STATIC_URL = '/static/'
+
+MEDIA_ROOT = os.path.join(Path(BASE_DIR).resolve().parent, 'media')
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
