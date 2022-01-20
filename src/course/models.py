1df26b8c41122a6de67abb68ad6c5ad0a174cd64
mojtabaakbari221b <mojtaba.akbari.221b@gmail.com>
2022-01-20 17:50:50 +0330

    course first migration

--- src/course/models.py
+++ src/course/models.py
@@ -4,13 +4,13 @@ from blog.models import Category
 from ckeditor.fields import RichTextField
 from django.contrib.auth.models import User
 from django.core.validators import MaxValueValidator, MinValueValidator
-from utilities.models_validatior import validate_video_extension
+from .utilities.models_validatior import validate_video_extension
 
 class Course(models.Model):
     title = models.CharField(max_length=1024)
     detail = RichTextField()
-    category = models.ForeignKey(Category)
-    teacher = models.ForeignKey(User)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    teacher = models.ForeignKey(User, on_delete=models.PROTECT)
     off = models.PositiveSmallIntegerField(
         default=0,
         validators=[
@@ -19,23 +19,23 @@ class Course(models.Model):
         ]
     )
     image = models.ImageField(upload_to="course/image")
-    date = models.DateTimeField(auto_now=True, auto_now_add=True)
+    date = models.DateTimeField(auto_now=True)
 
 class Content(models.Model):
-    course = models.ForeignKey(Course)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
     main_image = models.ImageField(upload_to="content/image")
     title = models.CharField(max_length=1024)
     detail = RichTextField()
     video = models.FileField(upload_to="course/video", validators=[validate_video_extension])
-    date = models.DateTimeField(auto_now=True, auto_now_add=True)
+    date = models.DateTimeField(auto_now=True)
 
 class Commnet(models.Model):
     name = models.CharField(max_length=256)
     email = models.EmailField()
-    user = models.ForeignKey(User, null=True)
+    user = models.ForeignKey(User, null=True, on_delete=models.PROTECT)
     text = models.CharField(max_length=1024)
-    course = models.ForeignKey(Course)
-    parent = models.ForeignKey('self' , related_name='parent_comment')
-    child = models.ForeignKey('self' , related_name='child_comment')
-    date = models.DateTimeField(auto_now=True, auto_now_add=True)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    parent = models.ForeignKey('self' , related_name='parent_comment', null=True, on_delete=models.CASCADE)
+    child = models.ForeignKey('self' , related_name='child_comment', null=True, on_delete=models.PROTECT)
+    date = models.DateTimeField(auto_now=True)
 
